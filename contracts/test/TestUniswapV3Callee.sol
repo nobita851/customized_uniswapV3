591f0faee271e71dbae8b84844200a5bf58b5caf
nobita851 <rktipu1995@gmail.com>
2025-01-28 03:36:28 +0530

    Remove flash loan related setup

--- contracts/test/TestUniswapV3Callee.sol
+++ contracts/test/TestUniswapV3Callee.sol
@@ -8,11 +8,11 @@ import '../libraries/TickMath.sol';
 
 import '../interfaces/callback/IUniswapV3MintCallback.sol';
 import '../interfaces/callback/IUniswapV3SwapCallback.sol';
-import '../interfaces/callback/IUniswapV3FlashCallback.sol';
+// import '../interfaces/callback/IUniswapV3FlashCallback.sol';
 
 import '../interfaces/IUniswapV3Pool.sol';
 
-contract TestUniswapV3Callee is IUniswapV3MintCallback, IUniswapV3SwapCallback, IUniswapV3FlashCallback {
+contract TestUniswapV3Callee is IUniswapV3MintCallback, IUniswapV3SwapCallback/* , IUniswapV3FlashCallback */ {
     using SafeCast for uint256;
 
     function swapExact0For1(
@@ -114,29 +114,29 @@ contract TestUniswapV3Callee is IUniswapV3MintCallback, IUniswapV3SwapCallback, 
             IERC20Minimal(IUniswapV3Pool(msg.sender).token1()).transferFrom(sender, msg.sender, amount1Owed);
     }
 
-    event FlashCallback(uint256 fee0, uint256 fee1);
+    // event FlashCallback(uint256 fee0, uint256 fee1);
 
-    function flash(
-        address pool,
-        address recipient,
-        uint256 amount0,
-        uint256 amount1,
-        uint256 pay0,
-        uint256 pay1
-    ) external {
-        IUniswapV3Pool(pool).flash(recipient, amount0, amount1, abi.encode(msg.sender, pay0, pay1));
-    }
+    // function flash(
+    //     address pool,
+    //     address recipient,
+    //     uint256 amount0,
+    //     uint256 amount1,
+    //     uint256 pay0,
+    //     uint256 pay1
+    // ) external {
+    //     IUniswapV3Pool(pool).flash(recipient, amount0, amount1, abi.encode(msg.sender, pay0, pay1));
+    // }
 
-    function uniswapV3FlashCallback(
-        uint256 fee0,
-        uint256 fee1,
-        bytes calldata data
-    ) external override {
-        emit FlashCallback(fee0, fee1);
+    // function uniswapV3FlashCallback(
+    //     uint256 fee0,
+    //     uint256 fee1,
+    //     bytes calldata data
+    // ) external override {
+    //     emit FlashCallback(fee0, fee1);
 
-        (address sender, uint256 pay0, uint256 pay1) = abi.decode(data, (address, uint256, uint256));
+    //     (address sender, uint256 pay0, uint256 pay1) = abi.decode(data, (address, uint256, uint256));
 
-        if (pay0 > 0) IERC20Minimal(IUniswapV3Pool(msg.sender).token0()).transferFrom(sender, msg.sender, pay0);
-        if (pay1 > 0) IERC20Minimal(IUniswapV3Pool(msg.sender).token1()).transferFrom(sender, msg.sender, pay1);
-    }
+    //     if (pay0 > 0) IERC20Minimal(IUniswapV3Pool(msg.sender).token0()).transferFrom(sender, msg.sender, pay0);
+    //     if (pay1 > 0) IERC20Minimal(IUniswapV3Pool(msg.sender).token1()).transferFrom(sender, msg.sender, pay1);
+    // }
 }
